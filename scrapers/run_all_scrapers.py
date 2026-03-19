6b2460a8fb07e3ccf2c3f5e0b1bbfe1730862fec
Mrrichards76 <krchds@gmail.com>
2026-03-19 12:50:59 -0400

    Update run_all_scrapers.py

--- scrapers/run_all_scrapers.py
+++ scrapers/run_all_scrapers.py
@@ -1,4 +1,5 @@
 import subprocess
 
+# Since run_all_scrapers.py is inside scrapers/, just call the scripts directly
 subprocess.run(["python", "github_scraper.py"])
 subprocess.run(["python", "angel_scraper.py"])
